77d5e03cc7fd71739badc5ba2eb6e1029656706f
Gray Morrow <tgrahammor@gmail.com>
2020-07-30 12:15:38 -0600

    Objective 3 labs complete

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,12 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 1.5;
+    num2 = 1.618033988709895;
+    num3 = 3.14159265;
+
+    average = ( num1 + num2 + num3 ) / 3;
+    System.out.println(average);
+  }
+}

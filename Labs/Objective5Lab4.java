98b76f0187008370fd3d25974ef9e71ce9f255c5
Gray Morrow <tgrahammor@gmail.com>
2020-08-05 09:55:49 -0600

    Last Objective 5 Lab

---
+++ Labs/Objective5Lab4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Objective5Lab4 {
+  public static void main(String[] args) {
+    Scanner kb = new Scanner(System.in);
+
+    System.out.println("Please enter a number: ");
+    
+    int userNum = kb.nextInt();
+
+    if(userNum % 2 == 1) {
+      System.out.println("The number is odd");
+    } else {
+      System.out.println("The number is even");
+    }
+
+    kb.close();
+  }
+}

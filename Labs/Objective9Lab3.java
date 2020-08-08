e18e424b0a5d90c36b03392c28cb54a56c9ccfc1
Gray Morrow <tgrahammor@gmail.com>
2020-08-08 13:35:35 -0600

    Objective 9 Labs

---
+++ Labs/Objective9Lab3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Objective9Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while ( true ) {
+      printMenu();
+      selection = scanner.nextInt();
+
+      if ( selection == 1 ) {
+          sayHello();
+      } else if ( selection == 2 ) {
+          favoriteFoods();
+      } else if ( selection == 3 ) {
+          System.out.println( "Goodbye" );
+          break;
+      } else {
+          System.out.println( "Invalid input" );
+      }
+    }
+  }
+  
+  public static void printMenu() {
+    System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
+  }
+
+  public static void sayHello() {
+    System.out.println("Hello Human");
+  }
+
+  public static void favoriteFoods() {
+    System.out.println( "Apples, Bananas, Coconuts" );
+  }
+}
